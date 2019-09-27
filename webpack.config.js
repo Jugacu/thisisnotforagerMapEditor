9538a12dd3e99bef66a250360f020976ff1825d1
Julián Sánchez <julian@jugacu.es>
2019-09-27 19:52:18 +0200

    first commit, base template

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const ROOT = path.resolve( __dirname, 'src' );
+const DESTINATION = path.resolve( __dirname, 'dist' );
+
+module.exports = {
+    context: ROOT,
+
+    entry: {
+        'main': './main.ts'
+    },
+    
+    output: {
+        filename: '[name].bundle.js',
+        path: DESTINATION
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js'],
+        modules: [
+            ROOT,
+            'node_modules'
+        ]
+    },
+
+    module: {
+        rules: [
+            /****************
+            * PRE-LOADERS
+            *****************/
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                use: 'source-map-loader'
+            },
+            {
+                enforce: 'pre',
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'tslint-loader'
+            },
+
+            /****************
+            * LOADERS
+            *****************/
+            {
+                test: /\.ts$/,
+                exclude: [ /node_modules/ ],
+                use: 'awesome-typescript-loader'
+            },
+            /**
+             * SCSSS
+             */
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    devtool: 'cheap-module-source-map',
+    devServer: {}
+};
+
