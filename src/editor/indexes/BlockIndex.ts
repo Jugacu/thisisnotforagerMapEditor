8ff58644e439de2dc14413ce47a1f0478bb56953
Julián Sánchez <julian@jugacu.es>
2019-10-04 21:31:48 +0200

    more blocks

--- src/editor/indexes/BlockIndex.ts
+++ src/editor/indexes/BlockIndex.ts
@@ -13,6 +13,8 @@ export default class BlockIndex {
         1: {name: 'grass', color: 0x63c74d},
         2: {name: 'sand', color: 0xfee761},
         3: {name: 'lava', color: 0xcf1020},
+        4: {name: 'mountain', color: 0x522900},
+        5: {name: 'java', color: 0xff3ad6},
     };
 
     public static getBlockData(id: number): IBlockIndexData {
